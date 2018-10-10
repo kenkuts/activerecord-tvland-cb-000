559accb51a8a016f062bb1cf8104cad9b1e10cae
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-10-10 05:06:01 +0000

    automatically backed up by learn

--- db/migrate/008_add_day_and_seasons_to_show.rb
+++ db/migrate/008_add_day_and_seasons_to_show.rb
@@ -2,5 +2,6 @@ class AddDayAndSeasonToShow < ActiveRecord::Migration
   def change
     add_column :shows, :day, :string
     add_column :shows, :seasong, :string
+    add_column :shows, :genre, :string
   end
 end
