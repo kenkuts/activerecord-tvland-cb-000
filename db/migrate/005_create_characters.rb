2a0e5a8c3fbe7fe2fae332cb3511f00d53315d23
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-10-10 02:48:03 +0000

    automatically backed up by learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,5 +1,7 @@
 class CreateCharacters < ActiveRecord::Migration[5.1]
-  create_table :characters do |t|
-    t.string :name
+  def change
+    create_table :characters do |t|
+      t.string :name
+    end
   end
 end
