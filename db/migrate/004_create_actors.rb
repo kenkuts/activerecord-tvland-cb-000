24937b93df72ccce1d9f851fc2381a4f100b0acb
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-10-10 02:47:47 +0000

    automatically backed up by learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -2,8 +2,10 @@ class CreateActors < ActiveRecord::Migration[5.1]
   #to make an additive change to a schema we create
   #a new migration, and then in the change method,
   #we make the change.
-  create_table :actors do |t|
-    t.string :first_name
-    t.string :last_name
+  def change
+    create_table :actors do |t|
+      t.string :first_name
+      t.string :last_name
+    end
   end
 end
